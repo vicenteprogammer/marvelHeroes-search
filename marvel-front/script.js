4fd23ce50d8e1243f4e1b2f7f292e61b3ea2bfe9
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-07-18 19:56:10 -0300

    model-window implementation

--- marvel-front/script.js
+++ marvel-front/script.js
@@ -1,6 +1,17 @@
-
+const modelWindow = document.getElementById('model-window')
 function search() {
 }
 
+function openWindow(){
+    modelWindow.classList.add('open');
+}
+
+function closeWindow(){
+    modelWindow.classList.remove('open');
+}
+
+
+const closeBtn = document.getElementById('close')
+closeBtn.addEventListener('click', closeWindow)
 const searchBtn = document.getElementById('search')
-searchBtn.addEventListener('click', search)
+searchBtn.addEventListener('click', openWindow)
